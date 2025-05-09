5e327ecca67021cb035806ba3db9821e25569dd9
tkshsbcue <kumar.tanay@somaiya.edu>
2025-05-09 20:24:59 +0530

    Initial Commit

---
+++ QuizzyTests/QuizzyTests.swift
@@ -0,0 +1,17 @@
+//
+//  QuizzyTests.swift
+//  QuizzyTests
+//
+//  Created by Kumar Tanay on 09/05/25.
+//
+
+import Testing
+@testable import Quizzy
+
+struct QuizzyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
